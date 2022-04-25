3259ef4e73d388b60baa4d7ab1d8df7cf058872b
BrodaJakub <jakub.broda@outlook.com>
2022-04-25 16:34:18 +0200

    Test SFML 1

--- statki/main.cpp
+++ statki/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-	std::cout << "Hello World!";
-	std::cout << "test";
-	return EXIT_SUCCESS;
+
+int main()
+{
+    //Fragment kodu do testu dzia³ania SFML
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Blue);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
