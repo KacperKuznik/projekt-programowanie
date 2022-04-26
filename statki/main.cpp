4e8399e719941c8d460bc19ac1ad3c29ded2394b
KacperKuznik <kuznikkacper@gmail.com>
2022-04-26 16:28:56 +0200

    game files

--- statki/main.cpp
+++ statki/main.cpp
@@ -1,18 +1,18 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "grid.cpp"
-
+#include "Game.cpp"
 using namespace std;
 
 
 
 int main()
 {
-    int width = 1200;
-    int height = 800;
+    int width = 1920;
+    int height = 1080;
     int grid_width = 500;
     float tile_width = grid_width/10;
-    sf::RenderWindow window(sf::VideoMode(width, height), "My window");
+    sf::RenderWindow window(sf::VideoMode(width, height), "statki");
     while (window.isOpen())
     {
         sf::Event event;
@@ -25,7 +25,9 @@ int main()
 
         Grid my_grid(window, grid_width, 0, 50);
         Grid enemy_grid(window, grid_width, grid_width + grid_width/10,50);
-
+        sf::Mouse mouse;
+        mouse.getPosition();
+        cout << mouse.getPosition().x << " " << mouse.getPosition().y << endl;
         window.display();
     }
 
