d4fbb455688c09fd5c9583c27aca763d01f89d3a
KacperKuznik <kuznikkacper@gmail.com>
2022-04-27 15:34:57 +0200

    click on tiles

--- statki/grid.cpp
+++ statki/grid.cpp
@@ -1,23 +1,40 @@
 #include <SFML/Graphics.hpp>
 #include "tile.cpp"
+#include <iostream>
 class Grid
 {
 private:
 	int rows = 10;
 	int cols = 10;
+	sf::RectangleShape **tiles;
 
 public:
-	Grid(sf::RenderWindow& window, int width, int x, int y) {
+	Grid( int width, int x, int y) {
 		int tile_width = width / rows;
+		tiles = new sf::RectangleShape * [rows];
 
 		for (int row = 0; row < rows; row++) {
+			tiles[row] = new sf::RectangleShape[cols];
+
 			for (int col = 0; col < cols; col++) {
 				sf::RectangleShape tile(sf::Vector2f(tile_width, tile_width));
 				tile.setPosition(x + tile_width *row, y + tile_width *col);
 				tile.setFillColor(sf::Color(240, 240, 240));
 				tile.setOutlineThickness(2.f);
 				tile.setOutlineColor(sf::Color(0, 0, 0));
-				window.draw(tile);
+				tiles[row][col] = tile;
+				
+			}
+		}
+
+	}
+	sf::RectangleShape** getTiles() {
+		return tiles;
+	};
+	void drawGrid(sf::RenderWindow& window) {
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
+				window.draw(tiles[row][col]);
 			}
 		}
 	}
--- statki/main.cpp
+++ statki/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "grid.cpp"
 #include "Game.cpp"
+
 using namespace std;
 
 
@@ -11,23 +12,42 @@ int main()
     int width = 1920;
     int height = 1080;
     int grid_width = 500;
-    float tile_width = grid_width/10;
+    int tile_width = grid_width/10;
+    char column_labels[10];
+    for (int i = 0; i < 10; i++) {
+        column_labels[i] = (char) i + 97;
+    }
+
+
     sf::RenderWindow window(sf::VideoMode(width, height), "statki");
+    Grid my_grid(grid_width, 0, 0);
     while (window.isOpen())
     {
+        
+
+
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-        }
-        window.clear(sf::Color::White);
 
-        Grid my_grid(window, grid_width, 0, 50);
-        Grid enemy_grid(window, grid_width, grid_width + grid_width/10,50);
-        sf::Mouse mouse;
-        mouse.getPosition();
-        cout << mouse.getPosition().x << " " << mouse.getPosition().y << endl;
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    std::cout << "Tile: " << column_labels[event.mouseButton.x / tile_width] << event.mouseButton.y / tile_width + 1 << endl;
+                    my_grid.getTiles()[event.mouseButton.x / tile_width][event.mouseButton.y / tile_width].setFillColor(sf::Color::Blue);
+                }
+            }
+        }
+
+        window.clear(sf::Color::White);
+        my_grid.drawGrid(window);
+        
+        //Grid enemy_grid(window, grid_width, grid_width + grid_width/10,0);
+       
+     
         window.display();
     }
 
