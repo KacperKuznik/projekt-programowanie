b27baf18aaf2a2f704745cc8693e88890604adef
BrodaJakub <jakub.broda@outlook.com>
2022-05-10 21:49:36 +0200

    Granice

--- statki/PlayerGrid.cpp
+++ statki/PlayerGrid.cpp
@@ -11,8 +11,15 @@ public:
 	void placeShip(Ship& ship, sf::Vector2f mouse) {
 		auto pos = getClickedPosition(mouse);
 		if (pos.row >= 0 && pos.col >= 0) {
-			sf::Vector2f xy = tiles[pos.row][pos.col].getPosition();
-			ship.setPos(xy);
+			if (pos.row + ship.size() > 10)
+			{
+
+			}
+			else
+			{
+				sf::Vector2f xy = tiles[pos.row][pos.col].getPosition();
+				ship.setPos(xy);
+			}
 		}
 	}
 };
